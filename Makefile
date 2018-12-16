5a35d0ebc22c2fc3e1a95c277e509f8d230a3254
Niklas Fasching <niklas.fasching@gmail.com>
2018-12-16 17:20:17 +0100

    Add github pages demo

--- Makefile
+++ Makefile
@@ -24,3 +24,12 @@ install:
 .PHONY: setup
 setup: install
 	git config core.hooksPath etc/githooks
+
+.PHONY: generate-gh-pages
+generate-gh-pages: install
+	cp -r etc/gh-pages .
+	cp $(shell go env GOROOT)/misc/wasm/wasm_exec.js gh-pages/wasm_exec.js
+	mv gh-pages/_js.go gh-pages/js.go
+	go get github.com/kr/pretty
+	GOOS=js GOARCH=wasm go build -o gh-pages/main.wasm gh-pages/js.go
+	rm gh-pages/js.go
---
+++ etc/gh-pages/_js.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+
+	"github.com/kr/pretty"
+	"github.com/niklasfasching/gowen"
+	_ "github.com/niklasfasching/gowen/lib/core"
+)
+
+func main() {
+	env := gowen.NewEnv(false)
+	js.Global().Call("gowenInitialized")
+
+	document := js.Global().Get("document")
+	in := document.Call("getElementById", "gowen-input")
+	out := document.Call("getElementById", "gowen-output")
+	js.Global().Set("gowenRun", js.NewCallback(func([]js.Value) {
+		nodes, err := gowen.Parse(in.Get("value").String())
+		if err != nil {
+			out.Set("textContent", fmt.Sprintf("%s", err))
+			return
+		}
+		results, err := gowen.EvalMultiple(nodes, env)
+		if err != nil {
+			out.Set("textContent", fmt.Sprintf("%s", err))
+			return
+		}
+		s := ""
+		for _, result := range results {
+			s += pretty.Sprint(result.ToGo()) + "\n"
+		}
+		out.Set("textContent", s)
+	}))
+
+	<-make(chan struct{}) // stay alive
+}
